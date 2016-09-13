26bb95d3d4aaea153696916c348efe4dd5a4dd0a
nikhil5467 <nikhilrocks374@gmail.com>
2016-09-14 00:21:52 +0530

    session5assignment2

---
+++ app1.js
@@ -0,0 +1,12 @@
+var personObj = {
+    
+    name : "Nikhil Kumar",
+    age : 25,
+    dob : "05-03-1991",
+    placeOfBirth : "PATNA"
+}
+
+console.log("Name :"+personObj.name);
+console.log("Age :"+personObj.age);
+console.log("Date of Birth :"+personObj.dob);
+console.log("Place of Birth :"+personObj.placeOfBirth);
